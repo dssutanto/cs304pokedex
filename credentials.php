5fe5c57b3370437c7f1b6526be8fc41365e7b26c
dssutanto <derrick_sutanto@msn.com>
2021-11-03 23:44:46 -0700

    Made presentable

---
+++ credentials.php
@@ -0,0 +1,5 @@
+<?php
+    $username = NULL;       // Username to your Oracle server
+    $pass = NULL;           // Password to your Oracle server
+    $instance = NULL;       // The name of the Oracle instance to which you have imported "pokedex.sql"
+?>
